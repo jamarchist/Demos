c5455b2472139610371a458a7f33afa58f4e8fea
jamarchist <none@none>
2011-01-27 20:44:40 -0600

    Added dependencies and changed assembly to a test assembly.

---
+++ PdfDocumentGenerator/PdfGenerationTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace PdfDocumentGenerator
+{
+    [TestFixture]
+    public class PdfGenerationTests
+    {
+        [Test]
+        public void CanGeneratePdfTable()
+        {
+            
+        }
+    }
+}
