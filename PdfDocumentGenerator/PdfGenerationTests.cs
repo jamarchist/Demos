2a1cad46f2775a93edb5fa6a6bc10a68c11afc85
jamarchist <none@none>
2011-01-27 22:36:26 -0600

    Wrote a basic test that generates a pdf.

--- PdfDocumentGenerator/PdfGenerationTests.cs
+++ PdfDocumentGenerator/PdfGenerationTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
 using NUnit.Framework;
 
 namespace PdfDocumentGenerator
@@ -11,7 +13,26 @@ namespace PdfDocumentGenerator
         [Test]
         public void CanGeneratePdfTable()
         {
-            
+            var doc = new Document();
+            var section = doc.AddSection();
+            var table = section.AddTable();
+
+            var column1 = table.AddColumn();
+            var column2 = table.AddColumn();
+
+            var row1 = table.AddRow();
+            row1.Cells[0].AddParagraph("Ryan");
+            row1.Cells[1].AddParagraph("30");
+
+            var row2 = table.AddRow();
+            row2.Cells[0].AddParagraph("Joe");
+            row2.Cells[1].AddParagraph("99");
+
+            var pdfRenderer = new PdfDocumentRenderer();
+            pdfRenderer.Document = doc;
+            pdfRenderer.RenderDocument();
+
+            pdfRenderer.Save(@"pdfsharptest.pdf");
         }
     }
 }
